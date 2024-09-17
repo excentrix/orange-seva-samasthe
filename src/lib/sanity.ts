b46ec2aafb837353c09ef3aa3d224df51a3dc3a6
SidKarthik <sa268592@gmail.com>
2024-09-17 23:52:56 +0530

    added env variable

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "5d677rbl",
+  projectId: process.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,44 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import Sitemap from "vite-plugin-sitemap";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    Sitemap({
-      hostname: "https://www.orangeseva.in",
-      exclude: ["/404"],
-      dynamicRoutes: [
-        "/",
-        "/about",
-        "/gallery",
-        "/testimonials",
-        "/about/mission",
-        "/about/team",
-        "/about/history",
-        "/about/impact",
-        "/projects",
-        "/donate",
-        "/contact",
-      ],
-      changefreq: "weekly",
-      priority: 0.8,
-      lastmod: new Date(),
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env variables based on the mode
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      "process.env": env,
     },
-  },
+    plugins: [
+      react(),
+      Sitemap({
+        hostname: env.VITE_HOSTNAME || "https://www.orangeseva.in",
+        exclude: ["/404"],
+        dynamicRoutes: [
+          "/",
+          "/about",
+          "/gallery",
+          "/testimonials",
+          "/about/mission",
+          "/about/team",
+          "/about/history",
+          "/about/impact",
+          "/projects",
+          "/donate",
+          "/contact",
+        ],
+        changefreq: "weekly",
+        priority: 0.8,
+        lastmod: new Date(),
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
