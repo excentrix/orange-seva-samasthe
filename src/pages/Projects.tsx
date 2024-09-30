3e22de103ae1586e630a5b680eb9aae545401d38
SidKarthik <sa268592@gmail.com>
2024-09-30 23:00:07 +0530

    fixed scroll in projects page and added tailwind scrollbar

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -100,14 +100,14 @@ const Projects: React.FC = () => {
         </AnimatePresence>
         <AnimatePresence>
           {active ? (
-            <div className="fixed inset-0 grid place-items-center z-[100] backdrop-blur-sm backdrop-brightness-50">
+            <div className="fixed inset-0 grid max-w-full w-full place-items-center z-[100] backdrop-blur-sm backdrop-brightness-50">
               <motion.button
                 key={`button-${active.title}-${id}`}
                 layout
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0, transition: { duration: 0.05 } }}
-                className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+                className="flex absolute top-6 right-6 lg:hidden items-center  justify-center bg-red-500 rounded-full h-8 w-8"
                 onClick={() => setActive(null)}
               >
                 <CloseIcon />
@@ -115,7 +115,7 @@ const Projects: React.FC = () => {
               <motion.div
                 layoutId={`card-${active._id}-${id}`}
                 ref={ref}
-                className="w-full max-w-[900px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+                className="w-11/12 rounded-lg max-w-[900px] h-[90vh] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
               >
                 <CardHeader>
                   <CardTitle className="text-2xl font-semibold mb-4">
@@ -123,74 +123,81 @@ const Projects: React.FC = () => {
                   </CardTitle>
                 </CardHeader>
 
-                <CardContent>
-                  <Carousel className="w-full max-w-4xl mx-auto">
-                    <CarouselContent>
-                      {active.images?.map((img) => (
-                        <CarouselItem key={img._id}>
-                          <Card className="relative overflow-hidden">
-                            <CardContent className="p-0">
-                              <img
-                                src={img.imageUrl}
-                                alt={img.alt}
-                                width={800}
-                                height={450}
-                                className="w-full h-[450px] object-cover"
-                              />
-                              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-6">
-                                <p className="text-white">{img.caption}</p>
-                              </div>
-                            </CardContent>
-                          </Card>
-                        </CarouselItem>
-                      ))}
-                    </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                  </Carousel>
+                <CardContent className="flex-grow overflow-y-auto overflow-x-hidden scrollbar-none">
+                  <div className="h-full flex flex-col">
+                    <Carousel
+                      className="w-full max-w-4xl mx-auto mb-4"
+                      opts={{
+                        loop: true,
+                      }}
+                    >
+                      <CarouselContent>
+                        {active.images?.map((img) => (
+                          <CarouselItem key={img._id}>
+                            <Card className="relative overflow-hidden">
+                              <CardContent className="p-0">
+                                <img
+                                  src={img.imageUrl}
+                                  alt={img.alt}
+                                  width={800}
+                                  height={450}
+                                  className="w-full h-[450px] object-cover"
+                                />
+                                <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-6">
+                                  <p className="text-white">{img.caption}</p>
+                                </div>
+                              </CardContent>
+                            </Card>
+                          </CarouselItem>
+                        ))}
+                      </CarouselContent>
+                      {/* <CarouselPrevious />
+                      <CarouselNext /> */}
+                    </Carousel>
 
-                  <p className="m-4">{active.description}</p>
+                    <p className="m-4 flex-grow pb-10">{active.description}</p>
+                  </div>
                 </CardContent>
               </motion.div>
             </div>
           ) : null}
         </AnimatePresence>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project) => {
-            console.log(project.images[0]);
-            return (
+          {projects.map((project) => (
+            <motion.div
+              layoutId={`card-${project._id}-${id}`}
+              key={project._id}
+              onClick={() => setActive(project)}
+              className=" bg-white dark:bg-neutral-800 rounded-xl shadow-md overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300"
+            >
               <motion.div
-                layoutId={`card-${project._id}-${id}`}
-                key={project._id}
-                onClick={() => setActive(project)}
-                className="bg-white dark:bg-neutral-800 rounded-xl shadow-md overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300"
+                layoutId={`img-${project._id}-${id}`}
+                className="w-full rounded-t-lg h-auto"
               >
-                <motion.div layoutId={`img-${project._id}-${id}`}>
-                  <img
-                    src={project.images[0].imageUrl || "/placeholder.jpg"}
-                    alt={project.images[0]?.alt || project.title}
-                    width={400}
-                    height={300}
-                    className="w-full h-48 object-cover"
-                  />
-                </motion.div>
-                <div className="p-4">
-                  <motion.h3
-                    layoutId={`title-${project._id}-${id}`}
-                    className="text-xl font-semibold mb-2"
-                  >
-                    {project.title}
-                  </motion.h3>
-                  <motion.p
-                    layoutId={`description-${project._id}-${id}`}
-                    className="text-gray-600 dark:text-gray-300 line-clamp-2"
-                  >
-                    {project.description}
-                  </motion.p>
-                </div>
+                <img
+                  src={project.images[0].imageUrl || "/placeholder.jpg"}
+                  alt={project.images[0]?.alt || project.title}
+                  width={400}
+                  height={300}
+                  className="w-full h-52 object-cover rounded-t-lg"
+                />
               </motion.div>
-            );
-          })}
+              <div className="p-4">
+                <motion.h3
+                  layoutId={`title-${project._id}-${id}`}
+                  className="text-xl font-semibold mb-2"
+                >
+                  {project.title}
+                </motion.h3>
+                <motion.p
+                  layoutId={`description-${project._id}-${id}`}
+                  className="text-gray-600 dark:text-gray-300 line-clamp-2"
+                >
+                  {project.description}
+                </motion.p>
+              </div>
+            </motion.div>
+          ))}
         </div>
       </div>
     </motion.div>
@@ -204,15 +211,15 @@ const CloseIcon = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 0.05 } }}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="30"
+      height="30"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="h-4 w-4 text-black"
+      className="h-5 w-5 text-white"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M18 6l-12 12" />
