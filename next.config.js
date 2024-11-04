cd308d0144126e41d92ac47d7e74c3111cfb94d3
Nidhishree G <nidhishreeg95@gmail.com>
2024-11-04 19:28:25 +0530

    Updated Impact Page Content

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Uncomment the lines below if you want to disable Turbopack
+  // experimental: {
+  //   turbo: {}, // Disable Turbopack
+  // },
+  images: {
+    domains: ["cdn.sanity.io", "picsum.photos", "d1muf25xaso8hp.cloudfront.net"],
+  },
+};
+
+module.exports = nextConfig;
